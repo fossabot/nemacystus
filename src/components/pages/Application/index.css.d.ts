5d25d48397f3db68161ed7520e99e35a1d86919f
Atnanasi <ys2000pro@gmail.com>
2019-06-08 21:26:59 +0900

    chore: run eslint

--- src/components/pages/Application/index.css.d.ts
+++ src/components/pages/Application/index.css.d.ts
@@ -1,9 +1,8 @@
 declare const styles: {
-  readonly "App": string;
-  readonly "logo": string;
-  readonly "logo-spin": string;
-  readonly "header": string;
-  readonly "link": string;
-};
-export = styles;
-
+  readonly App: string
+  readonly logo: string
+  readonly 'logo-spin': string
+  readonly header: string
+  readonly link: string
+}
+export = styles
